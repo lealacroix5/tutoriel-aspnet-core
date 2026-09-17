16c0c70bd28c46910085a0974fae1a71934f7bbc
Mililas <poulpimarvel@gmail.com>
2026-09-17 17:49:47 +0200

    creation du projet ASP.NET core MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tutoriel_aspnet_core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
