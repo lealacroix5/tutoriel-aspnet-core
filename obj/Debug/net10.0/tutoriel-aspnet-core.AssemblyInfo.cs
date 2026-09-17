d4ec33a09756dadbc02b7cf988c00cd849fe0c52
Mililas <poulpimarvel@gmail.com>
2026-09-17 19:14:54 +0200

    Ajout de la recherche de films

--- obj/Debug/net10.0/tutoriel-aspnet-core.AssemblyInfo.cs
+++ obj/Debug/net10.0/tutoriel-aspnet-core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tutoriel-aspnet-core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838f6af9e411359a0b97137d39bdd77b5bdae268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d2cc33d2486e5e0f3585e9e91bf23c3c87ed52")]
 [assembly: System.Reflection.AssemblyProductAttribute("tutoriel-aspnet-core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tutoriel-aspnet-core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
